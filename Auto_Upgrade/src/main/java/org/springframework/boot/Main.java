985d5ff5a00d732d7e2e57a0207262475ff95932
Admin <Admin@host.docker.internal>
2023-06-02 12:31:41 +0530

    git commit

--- Auto_Upgrade/src/main/java/org/springframework/boot/Main.java
+++ Auto_Upgrade/src/main/java/org/springframework/boot/Main.java
@@ -1,12 +1,21 @@
 package org.springframework.boot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
+	@RestController
+	  public class MyController {
 
+	    @GetMapping("/")
+	    public String home() {
+	      return "Hello, World!";
+	    }
+	  }
 
 }
